5ba846db641ae0da8d7a41b69c34e76ebe4e8f10
lythality <lythality@gmail.com>
2022-07-05 22:56:34 +0900

    implements - checking rule 7.1 (Octal constants shall not be used)

---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+import clang.cindex
+
+import re
+
+constants = []
+
+def traverse(n, i=0):
+    global constants
+
+    print(' ' * i, end="")
+    print(n.kind, end="")
+    print(" : ", end="")
+    print(n.spelling, end="")
+    print("")
+
+    if n.kind == clang.cindex.CursorKind.INTEGER_LITERAL:
+        constants.append(n)
+
+    for c in n.get_children():
+        traverse(c, i=i+1)
+
+
+def isOctet(text: str) -> bool:
+    return re.match(r"^0[0-7]+$", text)
+
+
+# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+if __name__ == '__main__':
+    clang.cindex.Config.set_library_file('C:/Program Files/LLVM/bin/libclang.dll')
+
+    afile = './test.c'
+
+    index = clang.cindex.Index.create()
+    translation_unit = index.parse(afile, ['-x', 'c++'])
+    traverse(translation_unit.cursor)
+
+    print("=== CONSTANTS ===")
+    for c in constants:
+        print(c.kind, end=" : ")
+        print(c.spelling or c.displayname, end=" :: ")
+        text = ""
+        for t in c.get_tokens():
+            print(t.spelling, end=" ")
+            text = text + t.spelling
+        print(c.type.spelling, end=" - ")
+        print(c.kind.name, end="")
+        print(" > OCTET IS USED" if isOctet(text) else "")
---
+++ test.c
@@ -0,0 +1,10 @@
+
+typedef int MY_INT;
+
+int main() {
+    int a, b, c;
+    a = 234580;
+    b = 31328;
+    c = a + b + 013;
+    return c;
+}
