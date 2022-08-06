48055ee842d63b09ec94d6df480ae04cea3a4af5
lythality <lythality@gmail.com>
2022-08-06 14:15:03 +0900

    implements - 9.[2,4,5] (checking array initializer)

--- src/rule_checker/__init__.py
+++ src/rule_checker/__init__.py
@@ -139,7 +139,7 @@ def get_array_size_list(n: CursorKind):
 
 def parse_array_initializer(n: CursorKind):
     if n.kind != CursorKind.INIT_LIST_EXPR:
-        return n.spelling
+        return getTokenString(n)
 
     ret = []
     for c in n.get_children():
@@ -362,11 +362,26 @@ class RuleChecker(SWorkspace):
             dimension = n.type.spelling.count("[")
             arr_size_list = list(reversed(get_array_size_list(n)))
             if dimension != len(arr_size_list):
-                # handled on rule 8.11
+                # handled on rule 8.11, so skip it.
                 continue
 
             if not check_arr_size(get_array_initializer(n), arr_size_list, 0):
                 print(" > array is not fully defined: "+n.spelling)
+
+        # checking rule 9.[2,4,5] - array init check
+        for n in self.var_decl:
+            if not is_array(n) or getTokenString(n).startswith("extern"):
+                continue
+            dimension = n.type.spelling.count("[")
+            arr_size_list = list(reversed(get_array_size_list(n)))
+            if dimension != len(arr_size_list):
+                if getTokenString(n).count("=") > 1:
+                    print(" > (9.5) array size is not given on index based initialized array: " + n.spelling)
+                continue
+
+            none_array = get_none_array_of(arr_size_list, 0)
+            initializer = get_array_initializer(n)
+            assigned_array = assign_initializer_to(get_none_array_of(arr_size_list, 0), get_array_initializer(n))
 
 
 def check_arr_size(component, arr_size_list, idx):
@@ -386,3 +401,57 @@ def check_arr_size(component, arr_size_list, idx):
     return True
 
 
+def get_none_array_of(arr_size_list, idx):
+    if idx >= len(arr_size_list):
+        return None
+    return [get_none_array_of(arr_size_list, idx+1) for _ in range(arr_size_list[idx])]
+
+
+# The initializer is an array where elements are string. Dimension depend on the array variable.
+def assign_initializer_to(none_array, initializer):
+    if type(initializer) != list:
+        print(" > (9.2) non-array is assigned to array (1)")
+        return None
+
+    idx = 0
+    for init in initializer:
+        if type(init) != list:
+            if "=" in init:
+                (index, value) = init.split("=")[0], init.split("=")[1]
+                index_array = re.split(r"\[|\]", index)
+                index_array = list(filter(None, index_array))
+                index_array = list(map(lambda x: int(x), index_array))
+                assign_at(none_array, index_array, value)
+                pass
+            else:
+                if idx >= len(none_array):
+                    print(" > initializer is bigger than the array")
+                    break
+                elif type(none_array[idx]) == list:
+                    print(" > (9.2) non-array is assigned to array (2)")
+                    break
+                if none_array[idx]:
+                    print(" > (9.4) re-initialize the array")
+                none_array[idx] = init
+                idx += 1
+        else:
+            if idx >= len(none_array):
+                print(" > initializer is bigger than the array")
+                break
+            assign_initializer_to(none_array[idx], init)
+            idx += 1
+    return none_array
+
+
+def assign_at(none_array, index_array, value):
+    if len(index_array) == 1:
+        if type(none_array[index_array[0]]) == list:
+            print(" > (9.2) non-array is assigned to array (3)")
+            return
+        elif none_array[index_array[0]]:
+            print(" > (9.4) re-initialize the array")
+        none_array[index_array[0]] = value
+    else:
+        none_array = none_array[index_array[0]]
+        index_array = index_array[1:]
+        assign_at(none_array, index_array, value)
--- test_res/test_array.c
+++ test_res/test_array.c
@@ -5,14 +5,19 @@ extern int ext_arr_002[];
 int dead_arr[4] = 3;
 
 int arr[4] = {0,1,2,3};
+int arr_indexed[2][2][2][2] = {[0][1][0][1]=1};
 
-int t[4] = {[1]=1, 2};
+int t[2] = {[0]=1, 2};
 
 int arr_2d[4][2]        = {{1,2},{3,4},{5,6},{7,8}};
 int arr_2d_linear[4][2] = {1,2,3,4,5,6,7,8};
+int arr_2d_dup[4][2]    = {{1,2},{3,4},{5,6},{7,8},[1][1]=9};
 
-int arr_2d_indexed[4][2] = {[1]={1,2},{3,4}};
-int arr_2d_2d_idxed[4][2] = {[1][1]={2},{4}};
+int arr_no_size[]       = {0,1};
+int arr_no_size_2[]     = {[0]=1};
+
+//int arr_2d_indexed[4][2] = {[1]={1,2},{3,4}};
+//int arr_2d_2d_idxed[4][2] = {[1][1]={2},{4}};
 
 int main() {
     int a, b, c;
