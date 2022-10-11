7d8ebe3fe6236ec941688164c9ca1cd4901c35df
lythality <lythality@gmail.com>
2022-10-12 01:16:15 +0900

    [implement] rule 10 shows violation location

--- src/rule_checker/type_check.py
+++ src/rule_checker/type_check.py
@@ -79,51 +79,51 @@ class ASTVisitor(ASTTraverser):
         global rule_checker
         if operator == "+" or operator == "-":
             if is_bool_type(lhs) or is_bool_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary +- 3 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary +- 3 @ " + getTokenString(n), n.location))
         elif operator == "*" or operator == "/":
             if is_bool_type(lhs) or is_bool_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary */ 3 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary */ 3 @ " + getTokenString(n), n.location))
             elif is_char_type(lhs) or is_char_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary */ 4 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary */ 4 @ " + getTokenString(n), s.location))
         elif operator == "%":
             if is_bool_type(lhs) or is_bool_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary % 3 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary % 3 @ " + getTokenString(n), n.location))
             elif is_char_type(lhs) or is_char_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary % 4 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary % 4 @ " + getTokenString(n), n.location))
             elif is_floating_type(lhs) or is_floating_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary % 1 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary % 1 @ " + getTokenString(n), n.location))
         elif operator == "<" or operator == ">" or operator == "<=" or operator == ">=":
             if is_bool_type(lhs) or is_bool_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary < > <= >= 3 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary < > <= >= 3 @ " + getTokenString(n), n.location))
         elif operator == "!" or operator == "&&" or operator == "||":
             if is_char_type(lhs) or is_char_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n), n.location))
             elif is_signed_type(lhs) or is_signed_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n), n.location))
             elif is_unsigned_type(lhs) or is_unsigned_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n), n.location))
             elif is_floating_type(lhs) or is_floating_type(rhs):
-                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n)))
+                rule_checker.add_violation(Violation(10, 1, "binary ! && || 2 @ " + getTokenString(n), n.location))
 
     def hook_array_subscript_expr(self, n, array, subscript):
         global rule_checker
         if is_bool_type(subscript):
-            rule_checker.add_violation(Violation(10, 1, "arr-sub 3 @ " + getTokenString(n)))
+            rule_checker.add_violation(Violation(10, 1, "arr-sub 3 @ " + getTokenString(n), n.location))
         elif is_char_type(subscript):
-            rule_checker.add_violation(Violation(10, 1, "arr-sub 4 @ " + getTokenString(n)))
+            rule_checker.add_violation(Violation(10, 1, "arr-sub 4 @ " + getTokenString(n), n.location))
         elif is_floating_type(subscript):
-            rule_checker.add_violation(Violation(10, 1, "arr-sub 1 @ " + getTokenString(n)))
+            rule_checker.add_violation(Violation(10, 1, "arr-sub 1 @ " + getTokenString(n), n.location))
 
     def hook_unary_operator(self, n, operand, operator):
         global rule_checker
         if is_bool_type(operand):
-            rule_checker.add_violation(Violation(10, 1, "unary 3 @ " + getTokenString(n)))
+            rule_checker.add_violation(Violation(10, 1, "unary 3 @ " + getTokenString(n), n.location))
         elif is_char_type(operand):
-            rule_checker.add_violation(Violation(10, 1, "unary 4 @ " + getTokenString(n)))
+            rule_checker.add_violation(Violation(10, 1, "unary 4 @ " + getTokenString(n), n.location))
         elif is_enum_type(operand):
-            rule_checker.add_violation(Violation(10, 1, "unary 5 @ " + getTokenString(n)))
+            rule_checker.add_violation(Violation(10, 1, "unary 5 @ " + getTokenString(n), n.location))
         elif is_unsigned_type(operand) and operator == "-":
-            rule_checker.add_violation(Violation(10, 1, "unary 8 @ " + getTokenString(n)))
+            rule_checker.add_violation(Violation(10, 1, "unary 8 @ " + getTokenString(n), n.location))
 
     def hook_conditional_operator(self, n, cond, true_expr, false_expr):
         pass
